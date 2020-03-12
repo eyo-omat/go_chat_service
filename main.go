ce6d5909e23754b91f2cc7e1d94f56a07ea1d161
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-03-12 11:53:16 +0000

    Added the auth function to the main.go file

--- main.go
+++ main.go
@@ -42,3 +42,57 @@ type userData struct {
 	Color string `json:"color"`
 	Name string `json:"name"`
 }
+
+func auth (w http.ResponseWriter, r *http.Request)  {
+	clientID := r.FormValue("clientID")
+	if	clientID == "" {
+		http.Error(w, "No client ID defined", http.StatusUnprocessableEntity)
+		return
+	}
+
+	// Public Room
+	publicRoomRegex := "^observable-room$"
+	// Private Room of the request user
+	userPrivateRoomRegex := fmt.Sprintf("^private-room-%s$", clientID)
+	// Private Rooms of other users except request user
+	otherUsersPrivateRoomRegex := fmt.Sprintf("^private-room-(?!%s$).+$", clientID)
+
+	claims := customClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 3).Unix(),
+		},
+		Client: clientID,
+		Channel: scalderoneID,
+		Data: userData{
+			Name: getRandomName(),
+			Color: getRandomColor(),
+		},
+		Permissions: map[string]permissionClaims{
+			publicRoomRegex: permissionClaims{
+				Publish: true,
+				Subscribe: true,
+			},
+			userPrivateRoomRegex: permissionClaims{
+				Publish: false,
+				Subscribe: true,
+			},
+			otherUsersPrivateRoomRegex: permissionClaims{
+				Publish: true,
+				Subscribe: false,
+			},
+		},
+	}
+
+	// Generate a new token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign token with secret key
+	tokenString, err := token.SignedString([]byte(scalderoneSecret))
+	if err != nil {
+		http.Error(w, "Unable to sign token", http.StatusUnprocessableEntity)
+		return
+	}
+
+	//Send to the user the token
+	w.Write([]byte(tokenString))
+}
